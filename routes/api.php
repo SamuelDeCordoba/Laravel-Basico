253a618e71f121605ed7b5cddade0bb24b691f66
Sammy <cordoba.samuel.5631@eam.edu.co>
2025-09-03 13:07:29 -0500

    database(migrations): table sales

--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController; // 👈 IMPORTANTE
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
 
 Route::get('/productos', [ProductoController::class, 'index']);
